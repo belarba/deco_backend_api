71de3090cf2f0e1f126bcfff7ee8519c0d76a58c
Daniel Belarmino <belarba@gmail.com>
2024-09-20 17:34:51 +0100

    controller inicial e configuarcao do redis

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::ProductsController < ApplicationController
+  def create
+    if params[:file].present?
+      file_path = Rails.root.join("tmp", params[:file].original_filename)
+      File.open(file_path, "wb") do |file|
+        file.write(params[:file].read)
+      end
+
+      DataProcessingWorker.perform_async(file_path.to_s)
+
+      render json: { status: "Started the processing" }, status: :ok
+    else
+      render json: { status: "File not found" }, status: :not_found
+    end
+  end
+end
