474ffc2c6c9719a320ab42290468d6196fdf813e
Daniel Belarmino <belarba@gmail.com>
2024-09-20 20:55:47 +0100

    implemented batches insert

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem 'mongoid', '~> 8.0.7'
 # Sidekiq for background job processing
 gem 'sidekiq'
 
+gem 'activerecord-import'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- app/workers/data_processing_worker.rb
+++ app/workers/data_processing_worker.rb
@@ -2,37 +2,69 @@ class DataProcessingWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'default', retry: 3
 
+  REMOVABLE_VALUES = ['BE', 'NK', 'FR', 'BE FE', 'BE NL', 'PT'].freeze
+  BATCH_SIZE = 1000
+
   def perform(file_path)
     # Lê o arquivo JSON
     data_array = JSON.parse(File.read(file_path))
+
+    # Inicializa arrays para processamento em lote
+    postgresql_batch = []
+    mongodb_batch = []
 
     # Limpa os dados para garantir que estejam em UTF-8
     data_array.each do |data|
       clean_data(data)
     end
 
-    # Normaliza os dados
-    data_array.each do |data|
+    # Normaliza os dados e armazena em lotes
+    data_array.each_with_index do |data, index|
       normalized_data = normalize_data(data)
 
-      # Salva no PostgreSQL
-      product = Product.create!(normalized_data)
+      # Regras de validação
+      if data["availability"] && normalized_data[:price] > 0
+        # Adiciona os dados ao lote
+        postgresql_batch << Product.new(normalized_data)
+        mongodb_batch << ExternalRecord.new(normalized_data)
+      end
 
-      # Salva no MongoDB
-      ExternalRecord.create!(normalized_data)
+      # Insere em lote quando atingir o tamanho BATCH_SIZE
+      if (index + 1) % BATCH_SIZE == 0
+        save_batches(postgresql_batch, mongodb_batch)
+        postgresql_batch.clear
+        mongodb_batch.clear
+      end
     end
+
+    # Salva os últimos registros que não completaram o lote
+    save_batches(postgresql_batch, mongodb_batch) unless postgresql_batch.empty?
+
+    # Remove o arquivo após o processamento
+    File.delete(file_path) if File.exist?(file_path)
   end
 
   private
 
+  def save_batches(postgresql_batch, mongodb_batch)
+    # Inserir em lote no PostgreSQL
+    Product.import(postgresql_batch, validate: false)
+
+    # Inserir em lote no MongoDB
+    ExternalRecord.collection.insert_many(mongodb_batch.map(&:attributes))
+  end
+
   def normalize_data(data)
+    shop_name = clean_invalid_chars(data["site"] || data["marketplaceseller"])
+    country = clean_invalid_chars(data["country"])
+
     {
-      country: data["country"],
+      country: country,
       brand: data["brand"],
       produtc_id: data["sku"].to_i,
       product_name: data["model"],
       product_category_id: data["categoryId"].to_i,
-      shop_name: data["site"] || data["marketplaceseller"],
+      shop_name: shop_name,
       price: data["price"].to_f,
       url: data["url"]
     }
@@ -50,4 +82,16 @@ class DataProcessingWorker
     end
   end
 
+  # Função para limpar dados indesejados do nome da loja
+  def clean_invalid_chars(received_text)
+    return nil if received_text.nil?
+
+    # Remover cada ocorrência dos valores inválidos
+    REMOVABLE_VALUES.each do |invalid_value|
+      received_text = received_text.gsub(/\b#{Regexp.escape(invalid_value)}\b/, '').strip
+    end
+
+    # Remove espaços duplicados que podem ter surgido após a limpeza
+    received_text.gsub(/\s+/, ' ')
+  end
 end
